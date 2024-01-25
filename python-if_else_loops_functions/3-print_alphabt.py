13ee13c4c00501c5f8196ac89f5d3314a41f0b9e
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-01-25 16:27:23 +0100

    picodestyle

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -2,4 +2,5 @@
 for letter_code in range(ord('a'), ord('z')+1):
     letter = chr(letter_code)
     if letter not in "qe":
-        print(letter, end="")
+        print("{}".format(letter), end='')
+
