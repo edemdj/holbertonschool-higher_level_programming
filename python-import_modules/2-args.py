a503c67b4121130a377779a670fcee8b8e2e5e86
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-01-26 13:59:14 +0100

    pycodestyle

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) == 1:
-        print("{} arguments. ".format(len(sys.argv) - 1))
-    elif len(sys.argv) == 2:
-        print("{} argument:" .format(len(sys.argv) - 1))
+
+    if len(sys.argv) - 1 == 0:
+        print("0 arguments.")
+    elif len(sys.argv) - 1 == 1:
+        print("1 argument:")
     else:
-        print("{} argument:" .format(len(sys.argv) - 1))
-    for i in range(len(sys.argv)):
-        if i == 0:
-            continue
-        print("{}: {}".format(i, sys.argv[i]))
+        print("{} arguments:".format(len(sys.argv) - 1))
+    for i in range(len(sys.argv) - 1):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
