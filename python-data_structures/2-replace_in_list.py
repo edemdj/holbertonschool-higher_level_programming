ee9f92c046af88d2c659712b88f38995d622662c
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-01-30 09:09:12 +0100

    function that replaces an element in a list at a specific position

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -7,5 +7,5 @@ def replace_in_list(my_list, idx, element):
     if idx > (len(my_list)):
         return my_list
     else:
-        my_list[3] = 9
+        my_list[idx] = element
     return new_list
