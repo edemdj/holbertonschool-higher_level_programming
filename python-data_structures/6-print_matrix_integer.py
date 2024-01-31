e32af64b020b1f809e512bb0c70022e18371272a
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-01-31 10:24:51 +0100

    pycodestyle

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-element = ""
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         row_output = " ".join(map(str, row))
         print("{}".format(row_output))
-            
-
