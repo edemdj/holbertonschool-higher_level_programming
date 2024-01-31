c85c15d33203ce65b819b443fcbadb097344578c
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-01-31 16:30:29 +0100

    pycodestyle

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -3,5 +3,5 @@ def only_diff_elements(set_1, set_2):
     list = [set_1, set_2]
     result = set()
     for i in list:
-        result =result.symmetric_difference(i)
+        result = result.symmetric_difference(i)
     return result
