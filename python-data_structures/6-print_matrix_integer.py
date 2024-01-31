1220750d4abca6bce02fe264634b0588f9de69fc
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-01-31 12:20:40 +0100

    pycodestyle

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for x in matrix:
-        for i in range(len(x)):
-            print("{}".format(x[i]), end="")
-            if i != len(x) - 1:
+    for row in matrix:
+        for i in range(len(row)):
+            print("{}".format(row[i]), end="")
+            if i != len(row) - 1:
                 print(" ", end="")
         print("")
