695cd56e53126d22b8c252551dbae3655026bab0
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-01-31 10:37:37 +0100

    pycodestyle

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for list in my_list:
-        print("{} ".format(list))
+        if list < 0:
+            return
+        else:
+            print("{} ".format(list))
