1c9e3779ae0e8b0db2e6370dc8b7add25df889a6
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-02-01 15:40:50 +0100

    pycodestyle

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -3,5 +3,5 @@ def search_replace(my_list, search, replace):
     new_list = my_list[:]
     for i, search in enumerate(my_list):
             if search == 2:
-                new_list[i] = replace
+                my_list[i] = replace
     return new_list
