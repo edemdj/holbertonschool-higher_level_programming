11304f1163f22b652fec8850d304468cd993bcec
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-02-05 15:57:20 +0100

    a class Square that defines a square by: (based on 0-square.py)

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,5 @@
+#!/usr/bin/python3
+"""empty class Square that defines a square:"""
 class Square:
     pass
 S = Square()
-print(S)
---
+++ python-classes/1-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""class Square that defines a square by: (based on 0-square.py) """
+class Square:
+    def __init__(self, size):
+        self._square__size = size
+S = Square('')
