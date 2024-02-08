166b01f2fe644d7112225586d3b7f903ce6be8b0
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-02-08 12:14:04 +0100

    pycodestyle

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 """class Square that defines a square by: (based on 0-square.py) """
+
+
 class Square:
+    """class Square that defines a square"""
     def __init__(self, size):
         self._square__size = size
+
+
 S = Square('')
