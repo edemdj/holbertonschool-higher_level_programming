6c9787f10b88a1655a797e1a0e7fc392ade91b8d
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-02-08 10:53:36 +0100

    pycodestyle

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 """ class Rectangle that defines a rectangle by: (based on 0-rectangle.py)"""
 class Rectangle:
+    """ defines an empty class rectangle """
     def __init__(self, width=0, height=0):
         self.height = height
         self.width = width
 
     @property
     def width(self):
+        """ retrieve width """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """ set widht with new value """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
@@ -19,15 +22,14 @@ class Rectangle:
 
     @property
     def height(self):
+        """ retrieve height """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """ set height with new value """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-
-    def __repr__(self):
-        return "{'_Rectangle__height': " + str(self.__height) + ", '_Rectangle__width': " + str(self.__width) + "}"
