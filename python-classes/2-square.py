23b0fcefdcc3388f315df2a6fd8ffc8267fae233
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-02-08 12:49:32 +0100

    pycodestyle

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,8 +1,10 @@
 #!usr/bin/python3
 """ class Square that defines a square by: (based on 1-square.py)"""
 class Square:
+    """class Square that defines a square"""
     def __init__(self, size=0):
-        self._square__size = size
+        """The __init__ method gets called after memory for the object is allocated"""
+        self.__size = size
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
