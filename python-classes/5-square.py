946dee9e0c3dc5a1dfff5bf0833c54c1068ff097
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-02-08 13:32:23 +0100

    pycodestyle

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
 """class Square that defines a square by: (based on 4-square.py)"""
+
+
 class Square:
+    """class Square that defines a square"""
     def __init__(self, size=0):
+        """instantiation"""
         self.size = size
 
     @property
     def size(self):
+        """return size"""
         return self.__size
 
     @size.setter
     def size(self, value):
+        """define special methods when modifying the attributes"""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -17,9 +23,11 @@ class Square:
         self.__size = value
 
     def area(self):
+        """return area"""
         return self.__size ** 2
-    
+
     def my_print(self):
+        """print the sqaure with #"""
         if self.__size == 0:
             print()
         if self.__size > 0:
