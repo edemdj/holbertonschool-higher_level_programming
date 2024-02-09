6e20f6725f6ccbdd865043daf2a616e3dcef2bf2
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-02-09 10:37:28 +0100

    pycodestyle

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,25 +1,19 @@
 #!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
-
 class TestMaxInteger(unittest.TestCase):
+
+
+    """ class for unittests """
     def test_max_integer(self):
-        self.assertIsNone(max_integer([]))
-
-        self.assertEqual(max_integer([1, 2, 3, 4, 5]), 5)
-
-        self.assertEqual(max_integer([-5, -4, -3, -2, -1]), -1)
-
-        self.assertEqual(max_integer([-5, 10, -3, 8, -1]), 10)
-
-        self.assertEqual(max_integer([1.5, 2.7, 3.9, 4.1]), 4.1)
-
-        with self.assertRaises(TypeError):
-            max_integer(['a', 'b', 'c'])
-
-        with self.assertRaises(TypeError):
-            max_integer([1, 2, 'a', 4])
-
-if __name__ == '__main__':
-    unittest.main()
+        """ check possible cases edge cases """
+        self.assertEqual(max_integer([]), None)
+        self.assertEqual(max_integer([3]), 3)
+        self.assertEqual(max_integer([-3]), -3)
+        self.assertEqual(max_integer([-1, -2, -3]), -1)
+        self.assertEqual(max_integer([15, 10, 5]), 15)
+        self.assertEqual(max_integer([15, 10, 5, -5, -10, 15]), 15)
+        self.assertEqual(max_integer([15, 15, 15]), 15)
