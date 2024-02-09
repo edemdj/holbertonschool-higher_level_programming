b46f9e2c9dc9443c8235255173524a8b4f7a1d44
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-02-09 15:56:53 +0100

    pycodestyle

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""prints a text with 2 new lines after each of these characters: ., ? and :"""
 def text_indentation(text):
     """function that prints a text"""
     if type(text) is not str:
