3fe0461a700968c1b795dc14d69d316034641620
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-02-14 12:27:42 +0100

    pycodestyle

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -3,7 +3,7 @@
 
 
 def inherits_from(obj, a_class):
-    """if the object is an instance of a class that inherited (directly or indirectly)"""
+    """if object is an instance of a class that inherited directly"""
     if (type(obj) is not a_class):
         return issubclass(type(obj), a_class)
     return False
