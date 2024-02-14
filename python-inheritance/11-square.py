ea85c84c4fa3574c715eb03727046a5f025c5d61
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-02-14 23:37:19 +0100

    pycodestyle

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
+""" Module """
+
+
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    """Square class, inherits from Rectangle"""
+
+
     def __init__(self, size):
+        """Instantiation"""
         super().__init__(size, size)
         self.__size = size
 
     def __str__(self):
+        """return Square"""
         return "[Square] {:d}/{:d}".format(self.__size, self.__size)
