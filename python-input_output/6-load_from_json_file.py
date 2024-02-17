8add42e08aacb72eb0d4f06765fe5b55aeb91e6b
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-02-17 12:16:22 +0100

    pycodestyle

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -6,4 +6,4 @@ import json
 def load_from_json_file(filename):
     """creates an Object from a “JSON file”:"""
     with open(filename, 'w') as f:
-        json.loads(f)
+        return json.loads(f)
