66bb936a6bc72aab299da4b2034671e6185e8070
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-02-18 09:36:13 +0100

    pycodestyle

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -4,9 +4,4 @@
 
 def class_to_json(obj):
     """create class"""
-    json_dict = {}
-    for attr_name in dir(obj):
-        attr_value = getattr(obj, attr_name)
-        if isinstance(attr_value, (list, dict, str, int, bool)):
-            json_dict[attr_name] = attr_value
-    return json_dict
+    return obj.__dict__
