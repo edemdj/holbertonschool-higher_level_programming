84eb1cc2c3e33ca1e07d076aec14a68eaa42f8b3
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-02-19 10:30:00 +0100

    pycodestyle

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Module"""
+
+
 import json
 import sys
 import os.path
@@ -9,14 +11,13 @@ load_file = __import__('6-load_from_json_file').load_from_json_file
 
 def add_item_to_list(args):
     try:
-        existing_list = load_from_json_file("add_item.json")
+        existing_list = load_file ("add_item.json")
     except FileNotFoundError:
         existing_list = []
 
     existing_list.extend(args)
-    save_to_json_file(existing_list, "add_item.json")
+    save_file(existing_list, "add_item.json")
 
 if __name__ == "__main__":
     args = sys.argv[1:]
     add_item_to_list(args)
-
