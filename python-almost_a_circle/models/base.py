12313d2981a40fc7ce26db9e371fcbec5cb580ce
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-02-22 09:34:49 +0100

    adding the static method def from_json_string(json_string): that returns the list of the JSON string representation json_string:

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -17,9 +17,25 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
     
+    @staticmethod
     def to_json_string(list_dictionaries):
         """Return the JSON string representation of list_dictionaries"""
         if not list_dictionaries:
             return "[]"
         return json.dumps(list_dictionaries)
-    
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Write the JSON string representation of list_objs to a file"""
+        if list_objs is None:
+            list_objs = []
+        filename = cls.__name__ + ".json"
+        with open(filename, 'w') as file:
+            file.write(cls.to_json_string([obj.to_dictionary() for obj in list_objs]))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Return the list of dictionaries from the JSON string representation"""
+        if json_string is None or json_string == "":
+            return []
+        return json.loads(json_string)
