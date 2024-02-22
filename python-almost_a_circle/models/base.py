85d26d0c900cbbb00a38ee6a0cf885c23e7f8090
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-02-22 10:41:13 +0100

    pycodestyle

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -5,8 +5,6 @@ import json
 
 class Base:
     """Base class"""
-
-
     __nb_objects = 0
 
     def __init__(self, id=None):
@@ -16,7 +14,7 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = self.__nb_objects
-    
+
     @staticmethod
     def to_json_string(list_dictionaries):
         """Return the JSON string representation of list_dictionaries"""
@@ -31,25 +29,26 @@ class Base:
             list_objs = []
         filename = cls.__name__ + ".json"
         with open(filename, 'w') as file:
-            file.write(cls.to_json_string([obj.to_dictionary() for obj in list_objs]))
+            dict_list = [obj.to_dictionary() for obj in list_objs]
+            json_string = cls.to_json_string(dict_list)
+            file.write(json_string)
 
     @staticmethod
     def from_json_string(json_string):
-        """Return the list of dictionaries from the JSON string representation"""
+        """Return the list of dictionaries from the JSON string"""
         if json_string is None or json_string == "":
             return []
         return json.loads(json_string)
-    
+
     @classmethod
     def create(cls, **dictionary):
         """Create an instance with all attributes set from the dictionary"""
         if cls.__name__ == 'Rectangle':
-            dummy_instance = cls(1, 1)  # Create a dummy Rectangle instance
+            dummy_instance = cls(1, 1)
         elif cls.__name__ == 'Square':
-            dummy_instance = cls(1)      # Create a dummy Square instance
+            dummy_instance = cls(1)
         else:
             raise ValueError("Unsupported class type")
-        
-        dummy_instance.update(**dictionary)  # Update attributes from the dictionary
+
+        dummy_instance.update(**dictionary)
         return dummy_instance
-    
