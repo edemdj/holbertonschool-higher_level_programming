945a51975b0c290fa299042c4a4b5f262728becc
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-02-23 23:48:58 +0100

    pycodestyle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -87,7 +87,7 @@ class Rectangle(Base):
 
     def __str__(self):
         """prints a rectangle using '#'"""
-        return f"[Rectangle] ({self.id}); {self.x}/{self.y} - {self.width}/{self.height}" 
+        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}" 
     
     def update(self, *args, **kwargs):
         """Update attributes with provided arguments"""
