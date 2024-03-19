d861ec15268c0f74b2f765d1c698b7f4a16ca8fc
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-03-19 16:14:46 +0100

    script that lists all shows contained in hbtn_0d_tvshows

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
-#!/usr/bin/env bas
+#!/usr/bin/env bash
 -- script that lists all cities contained in the database
 SELECT cities.id, cities.name,states.name FROM cities
 JOIN states ON cities.state_id=states.id
