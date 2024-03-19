c7186afeb68d5f8367d7b30940e57d2433b4545a
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-03-19 15:26:46 +0100

    fix files

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 -- script that lists all the cities of California 
-USE hbtn_0d_usa;
-
-SELECT * 
+SELECT id, name
 FROM cities
 WHERE state_id = (SELECT id FROM states WHERE name = 'California')
 ORDER BY id ASC;
