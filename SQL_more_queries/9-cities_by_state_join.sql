70e329f93e04ca98e072e9e83d79ac121d6c04c2
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-03-19 15:33:33 +0100

    script that lists all cities contained in the database

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,5 @@
 #!/usr/bin/env bas
 -- script that lists all cities contained in the database
-SELECT cities.id, cities.name AS city_name, states.name AS state_name
-FROM cities
-JOIN states ON cities.state_id = states.id
-ORDER BY cities.id ASC;
+SELECT cities.id, cities.name,states.name FROM cities
+JOIN states ON cities.state_id=states.id
+ORDER BY cities.id;
