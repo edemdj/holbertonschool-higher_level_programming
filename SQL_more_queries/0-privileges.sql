0e7b54e6516982c4b8acf361bbc82791cdf4df59
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-03-19 10:22:08 +0100

    fix files

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,6 @@
+#script that lists all privileges of the MySQL users user_0d_1 and user_0d_2
+-- For user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- For user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
---
+++ SQL_more_queries/3-force_name.sql
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+-- script that creates the table force_name
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    Nom VARCHAR(256) NOT NULL
+);
