b09b7c255c7e50594c6a859ac716da79683e21b3
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-03-20 09:28:18 +0100

    script that lists all shows contained in hbtn_0d_tvshows without a genre linked.

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+-- script that lists all genres from hbtn_0d_tvshows and displays the number of shows
+SELECT tv_show_genres.genre_id AS genre,
+       COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM tv_show_genres
+JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+GROUP BY tv_show_genres.genre_id
+ORDER BY number_of_shows DESC;
