3d5d20ee2ea38c1e4b9a8edf5359204955fad448
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-03-20 09:59:17 +0100

    script that lists all genres from hbtn_0d_tvshows

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 -- script that lists all genres from hbtn_0d_tvshows and displays the number of shows
-SELECT tv_show_genres.genre_id AS genre,
+SELECT tv_genres.name AS genre,
        COUNT(tv_show_genres.show_id) AS number_of_shows
-FROM tv_show_genres
-JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-GROUP BY tv_show_genres.genre_id
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY genre
 ORDER BY number_of_shows DESC;
