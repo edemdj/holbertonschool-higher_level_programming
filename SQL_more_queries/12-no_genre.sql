81236a261e631dc3b7e8485ce843075d72ec7bb7
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-03-20 09:30:59 +0100

    script that lists all shows contained in hbtn_0d_tvshows without a genre linked.

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 -- script that lists all shows contained in hbtn_0d_tvshows without a genre linked.
-SELECT genres.name AS genre,
-       COUNT(tv_show_genres.show_id) AS number_of_shows
-FROM tv_show_genres
-JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-JOIN genres ON tv_show_genres.genre_id = genres.id
-GROUP BY tv_show_genres.genre_id
-ORDER BY number_of_shows DESC;
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title ASC;
