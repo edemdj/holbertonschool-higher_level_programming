82d8eed58fcb591bb83454f49f09dc1af360451a
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-05-13 23:32:26 +0200

    fix file

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -7,6 +7,7 @@ def roman_to_int(roman_string):
     for i in range(len(roman_string)):
 
         if i != len(roman_string)-1 and roman[roman_string[i]] < roman[roman_string[i+1]]:
+            
             num += roman[roman_string[i]]*-1
         else:
             num += roman[roman_string[i]]
