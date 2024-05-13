39ca1909f43e57c046983b92a0957fa4aa565a7d
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-05-13 16:18:34 +0200

    fix 1-searche _replace.py

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = my_list[:]
-    for i, search in enumerate(my_list):
-        if search == 2:
+    for i, item in enumerate(my_list):
+        if item == search:
             new_list[i] = replace
     return new_list
