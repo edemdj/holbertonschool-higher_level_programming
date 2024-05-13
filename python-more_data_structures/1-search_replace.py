1cac80313a85e971003872c4d3802c1289ab0af1
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-05-13 16:15:25 +0200

    fix 1-searche _replace.py

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -2,6 +2,6 @@
 def search_replace(my_list, search, replace):
     new_list = my_list[:]
     for i, search in enumerate(my_list):
-            if search == 2:
-                new_list[i] = replace
+        if search == 2:
+            new_list[i] = replace
     return new_list
