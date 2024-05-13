1917ec5df3d4eef89feecb2bd6641591a67e5c40
edemdj <146949154+edemdj@users.noreply.github.com>
2024-05-13 22:30:20 +0200

    Delete python-input_output directory

--- python-input_output/0-read_file.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-""" Module """
-
-
-def read_file(filename=""):
-    """Reads and prints the content of a text file."""
-    with open(filename, 'r', encoding='utf-8') as file:
-        for line in file:
-            print(line, end='')
--- python-input_output/8-class_to_json.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""Module"""
-
-
-def class_to_json(obj):
-    """create class"""
-    return obj.__dict__
