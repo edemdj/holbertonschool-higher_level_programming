90410498f9aa3fa9a29ac256bc10d9290dc6be10
edemdj <146949154+edemdj@users.noreply.github.com>
2024-05-13 22:28:11 +0200

    Delete SQL_more_queries directory

--- SQL_more_queries/16-shows_by_genre.sql
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
--- script that lists all shows, and all genres linked to that show, 
-SELECT tv_shows.title, COALESCE(tv_genres.name, NULL) AS name
-FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-ORDER BY tv_shows.title ASC, tv_genres.name ASC;
--- SQL_more_queries/8-cities_of_california_subquery.sql
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
--- script that lists all the cities of California 
-SELECT id, name
-FROM cities
-WHERE state_id = (SELECT id FROM states WHERE name = 'California')
-ORDER BY id ASC;
