e09fe0aed317b07dfb5a318769de7eccb07406a3
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-05-21 10:47:55 +0200

    Text indentation

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""prints a text with 2 new lines after each of these characters: ., ? and :"""
+
+
+def text_indentation(text):
+    """function that prints a text"""
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    i = 0
+    for char in text:
+        if i == 0:
+            if char == " ":
+                continue
+            else:
+                i = 1
+        if i == 1:
+            if char in [".", "?", ":"]:
+                print((char))
+                print()
+                i = 0
+            else:
+                print((char), end=(""))
