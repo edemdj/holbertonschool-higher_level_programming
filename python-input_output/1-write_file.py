b112ba84c518e08135532c74279a41b55c9d29d9
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-05-27 11:10:42 +0200

    pycodestyle

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -3,7 +3,7 @@
 
 
 def write_file(filename="", text=""):
-    """Writes a string to a text file (UTF-8) and returns number of characters"""
+    """Writes to a text file (UTF-8) and returns number of characters"""
     with open(filename, 'w', encoding='utf-8') as file:
         num_chars_written = file.write(text)
     return num_chars_written
