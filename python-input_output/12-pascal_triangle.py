b0f1cd6f0844787bfed3bc7c80b855dec64d0467
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-05-28 10:40:47 +0200

    Pascal's Triangle

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module"""
+
+
+def pascal_triangle(n):
+    """Represents a pascal triangle"""
+    new_matrix = []
+    if n <= 0:
+        return new_matrix
+    new_matrix = [[1]]
+    for i in range(1, n):
+        rows = [1]
+        for j in range(len(new_matrix[i - 1]) - 1):
+            current = new_matrix[i - 1]
+            rows.append(new_matrix[i - 1][j] + new_matrix[i - 1][j + 1])
+        rows.append(1)
+        new_matrix.append(rows)
+    return new_matrix
