6a70e9c924b1be8d2520eefa3cd2f6bb06064eec
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-07-16 12:04:36 +0200

    Extending Dynamic Data Display to Include SQLite in Flask

---
+++ python-server_side_rendering/task_03_files.py
@@ -0,0 +1,57 @@
+from flask import Flask, render_template, request
+import json
+import csv
+
+app = Flask(__name__)
+
+def read_json_file(filepath):
+    with open(filepath, 'r') as file:
+        return json.load(file)
+
+def read_csv_file(filepath):
+    products = []
+    with open(filepath, 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            products.append({
+                "id": int(row["id"]),
+                "name": row["name"],
+                "category": row["category"],
+                "price": float(row["price"])
+            })
+    return products
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/products')
+def products():
+    source = request.args.get('source')
+    product_id = request.args.get('id')
+
+    if source == 'json':
+        products = read_json_file('products.json')
+    elif source == 'csv':
+        products = read_csv_file('products.csv')
+    else:
+        return render_template('product_display.html', error="Wrong source")
+
+    if product_id:
+        product_id = int(product_id)
+        products = [p for p in products if p['id'] == product_id]
+        if not products:
+            return render_template('product_display.html', error="Product not found")
+
+    return render_template('product_display.html', products=products)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
